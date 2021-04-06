b718e8ccce5ea33e159dbad48c75e99b129fb39d
Bukola Ajanaku <bukola@Bukolas-MacBook-Pro.local>
2021-04-06 00:39:15 -0400

    Corrected medical tables and cleaned up longMedical script.

--- scripts/longMedical.R
+++ scripts/longMedical.R
@@ -13,9 +13,12 @@ library(reshape2)
 library(gt)
 
 # Load data
-demo_df <- read.csv(file = "~/Box Sync/medical_pnc/n9498_demo_sex_race_ethnicity_dob.csv")
-screen_df <- read.csv(file = "~/Box Sync/medical_pnc/Longitudinal Medical/fullscreen_medhistory_oracle_n9498.csv")
-date_df <- read.csv(file= "~/Box Sync/medical_pnc/pnc_longitudinal_diagnosis_n749_20210112.csv")
+demo_df <- read.csv(file = "~/Box Sync/medical_pnc/
+n9498_demo_sex_race_ethnicity_dob.csv")
+screen_df <- read.csv(file = "~/Box Sync/medical_pnc/Longitudinal Medical/
+fullscreen_medhistory_oracle_n9498.csv")
+date_df <- read.csv(file= "~/Box Sync/medical_pnc/
+pnc_longitudinal_diagnosis_n749_20210112.csv")
 
 # Recalculate ntimepoints (a lot of erroneous zeros)
 for (bblid in unique(date_df$bblid)) {
@@ -24,9 +27,7 @@ for (bblid in unique(date_df$bblid)) {
 }
 
 # Select only the first and last timepoints in date_df (think about how to
-# use 'ntimepoints'!
 # How Ellyn did it: date_df <- date_df[date_df$timepoint == ‘t1’ |
-# date_df$timepoint == paste0(’t’, date_df$ntimepoints), ]
 clinical_df <- date_df[date_df$timepoint == "t1" | mapply(grepl,
 date_df$ntimepoints, date_df$timepoint),]
 
@@ -35,7 +36,6 @@ clinical_df$diagnosis <- recode(clinical_df$diagnosis, "psy" = "PS")
 
 # reshape2::dcast() date_df so that the two values of 'timepoint' each get
 # their own column
-# Step 1: recode the timepoint column | Step 2: reshape. need value.var
 clinical_df$timepoint <- recode(clinical_df$timepoint, "t2" = "tfinal",
 "t3" = "tfinal", "t4" = "tfinal", "t5" = "tfinal", "t6" = "tfinal")
 clinical_df <- reshape2::dcast(clinical_df, bblid ~ timepoint,
@@ -65,8 +65,6 @@ date_df$timepoint),]
 # vector of length two
 
 ageFirstLast <- function(bblid){
-#  bblid <- date_df[i,"bblid"]; This is only relevant for sapply but I for loop.
-#  Ellyn: Anything written for a for loop can be written for a sapply.
 	forBblid <- date_df[,c("bblid", "age")]
 	forBblid <- forBblid[forBblid$bblid == bblid,]
 	aget1 <- min(forBblid$age)
@@ -82,30 +80,11 @@ ageFirstLast <- function(bblid){
 # respectively).
 
 colnames(screen_df)[colnames(screen_df) == "BBLID"] <- "bblid"
-# Tries to get rid of the duplication error.
+  # Tries to get rid of the duplication error.
 date_df2 <- date_df[duplicated(date_df$bblid),c("bblid", "dob")]
 screen_df <- merge(date_df2, screen_df, by = "bblid")
 screen_df$DOMHSCREEN <- as.Date(screen_df$DOMHSCREEN, format = "%m/%d/%y")
 screen_df$age <- (as.numeric(screen_df$DOMHSCREEN - screen_df$dob))/365.25
-
-# 1st method for assessNumber. Fail! Originally wanted to use it to edit the
-# order of the ages which I would just go back to timestamp. Did not work
-# because ages were manipulated, repeated, or just plain wrong. Next part was to
-# add the "timpoint" part and edit in function but it would not sapply for
-# everyone and would not save while in function form. I did not know how to
-# sapply with specifications like in a for loop so I just foor looped and
-# changed the function:
-# assessNumber <- function(i){
-#  bblid <- screen_df[i,"bblid"]
-#  forBblid <- screen_df[,c("bblid", "age", "timepoint")]
-#  forBblid <- forBblid[forBblid$bblid == bblid,]
-#  forBblid$timepoint <- order(forBblid$age)
-#  return(forBblid)
-#}
-# ahhh <- sapply(1:nrow(screen_df), assessNumber)
-
-
-# Corrected but longer version:
 screen_df$timepoint <- NA
 assessNumber <- function(bblid){
   forBblid <- screen_df[screen_df$bblid == bblid, c("bblid", "age")]
@@ -118,22 +97,9 @@ screen_df[screen_df$bblid == bblid, "timepoint"] <- assessNumber(bblid)
 
 ################################################################################
 
-# sapply() your ageFirstLast() function
-
-# assessNumber use to filter screen_df for the first time point
-# need to merge in dob to all of the dataframes in order to calc age
-# if there is a single person that doesn't have a medical followup for one of the medical
-# conditions, only put the first age in the tables that we are creating.
-# Medical table that we are making will only have the first ages.
-# Clinical table will look like Ellyn's with the first and last ages.
-# Merge dob directly into screen_df and then do the assessNumber thingy
-# gtsummary ** : need like 50 tables, use a for loop -> html table output
-
+# sapply() your ageFirstLast() function. ~Used merge and then a for loop instead.
 
 final_df <- merge(demo_df, clinical_df, by = "bblid")
-#ERROR:
-# final_df[c("firstAge", "lastAge")] <- (sapply(final_df$bblid), ageFirstLast))
-
 final_df[c("firstAge", "lastAge")] <- NA
 for(bblid in final_df$bblid){
 final_df[final_df$bblid == bblid,]$firstAge <- ageFirstLast(bblid)[1]
@@ -149,44 +115,50 @@ final_df$Diagnosis <- recode(final_df$t1_tfinal, 'other_TD'='OP-TD',
   'TD_TD'='TD-TD', 'TD_PS'='TD-PS', 'PS_TD'='PS-TD', 'PS_PS'='PS-PS')
 final_df$Diagnosis <- ordered(final_df$Diagnosis, c('TD-TD', 'TD-OP', 'TD-PS',
   'OP-TD', 'OP-OP', 'OP-PS', 'PS-TD', 'PS-OP', 'PS-PS'))
+
+
 medical_Table <- final_df %>% select("sex", "race", "firstAge", "lastAge",
 "Diagnosis")
-
-plotClinical <- medical_Table %>% tbl_summary(by = Diagnosis, label =
+# Creates clinical table from all bblids in medical_Table using first and
+# last ages.
+tabClinical <- medical_Table %>% tbl_summary(by = Diagnosis, label =
 list(sex ~ "Sex", race ~ "Race", firstAge ~ "First Age", lastAge ~ "Final Age"))
-plotClinical <- plotClinical %>% as_gt()
-gtsave(plotClinical,"plotClinical.html", "~/Documents/PennBBL/pncMedical/plots")
+tabClinical <- tabClinical %>% as_gt()
+gtsave(tabClinical,"tabClinical.html", "~/Documents/PennBBL/pncMedical/tables")
 
+# Merge date_df and screen_df so that you only get the bblids in date_df that
+# are in screen_df
 newScreen <- merge(screen_df, final_df, by = "bblid")
 
-#subsetting screen_df for only the first timepoints.
+# Subsetting screen_df for only the first timepoints.
 newScreen <- newScreen[newScreen$timepoint == 1,]
+
+# Cleaning up newScreen so that only relevant columns are in the for loop.
 drop <- c("dob.x","dob.y","SID","OTHERID","OTHER_SITE", "PROTOCOL", "DOMHSCREEN",
-"timepoint", "ethnicity", "dob.y", "tfinal", "t1_tfinal", "t1", "lastAge", "age",
+"timepoint", "ethnicity", "dob.y", "tfinal", "t1", "lastAge", "age",
 "Diagnosis")
 newScreen <- newScreen[,!mapply(grepl,"NOTES", colnames(newScreen))]
 newScreen <- newScreen[,!(names(newScreen) %in% drop)]
 
+# Recording variables for the table.
+newScreen$t1_tfinal <- recode(newScreen$t1_tfinal, 'other_TD'='OP-TD',
+  'other_other'='OP-OP', 'other_PS'='OP-PS', 'TD_other'='TD-OP', 'PS_other'='PS-OP',
+  'TD_TD'='TD-TD', 'TD_PS'='TD-PS', 'PS_TD'='PS-TD', 'PS_PS'='PS-PS')
+
+# For loop the screen_df for the diagnosis and include only the rows that have
+# valid values (Y/N) and only use the first ages. A table per medical diagnosis.
 for(i in colnames(newScreen)[2:22]){
   now <- na.omit(newScreen[newScreen[,2] %in% c("Y","N"),
-  c("bblid", i, "sex", "race", "firstAge")])
+  c("bblid", i, "sex", "race", "firstAge", "t1_tfinal")])
   now <- now[,!colnames(now) == "bblid"]
+  now <- now[,!colnames(now) == i]
   assign(paste0("print", i), now)
-  print <- now %>% tbl_summary(by = colnames(now)[2], label =
-  list(sex ~ "Sex", race ~ "Race", firstAge ~ "First Age"))
+  print <- now %>% tbl_summary(by = colnames(now)[4], label =
+  list(sex ~ "Sex", race ~ "Race", firstAge ~ "First Age")) %>%
+  modify_spanning_header(paste("stat", 1:nrow(unique(now[4])), sep = "_") ~ i)
   print <- print %>% as_gt()
+#  %>% tab_style(style = cell_text(font = "Times", locations = all)
   name <- paste0("print", i)
   namer <- paste(name, "html", sep = ".")
-  gtsave(print, namer, "~/Documents/PennBBL/pncMedical/plots/medPlots")
+  gtsave(print, namer, "~/Documents/PennBBL/pncMedical/tables/medTables")
 }
-
-
-# ..........
-
-# merge date_df and screen_df so that you only get the bblids in date_df that are
-# in screen_df
-
-
-# ..........
-#for loop the screen_df for the diagnosis and include only the rows that have
-# valid values (Y/N) and only use the first ages. A table per medical diagnosis.
